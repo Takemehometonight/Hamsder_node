afd6c25e4e02844811590f67c5809a3bfb246c74
Eric Fang <jiexuanfang@gmail.com>
2023-04-23 01:38:13 +0000

    start.sh now initialize and runs the mysql server on local environment: have to initialize database manually

--- start.sh
+++ start.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# Download the MySQL Docker image
-docker pull mysql
-
 # Run the MySQL container
-docker run -d --name mysql -e MYSQL_ROOT_PASSWORD=password -p 3306:3306 mysql
+docker run --name mysql-container -e MYSQL_ROOT_PASSWORD=password -p 3306:3306 -d mysql
 
 # Wait for the MySQL server to start
 echo "Waiting for MySQL server to start..."
-while ! docker exec mysql mysqladmin ping --silent &>/dev/null; do
+while ! docker exec mysql-container mysqladmin ping --silent &>/dev/null; do
     sleep 1
 done
+echo "MySQL server started!"
 
